6ec97205e0f0774220d3e20abf0ad6b7facf1217
Zariy7 <nazariy.hereha@gmail.com>
2023-11-19 22:42:52 +0100

    exercise complete

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let nome = prompt("Please, enter your name.");
+console.log(nome);
+let surname = prompt("Please, enter your surname.");
+console.log(surname);
+let favColor = prompt("Please, enter your favorite color.");
+console.log(favColor);
+
+document.getElementById("unsafe-password").innerHTML = nome+surname+favColor;
